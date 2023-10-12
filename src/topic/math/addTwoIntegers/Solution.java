6484b5e7ebe0def3f4b26ef9b2503743b711fdbf
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-10-12 21:02:23 +0600

    task #2235 is solved (return the sum of the two integers)

---
+++ src/topic/math/addTwoIntegers/Solution.java
@@ -0,0 +1,13 @@
+package topic.math.addTwoIntegers;
+
+//Given two integers num1 and num2, return the sum of the two integers
+public class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+        System.out.println(s1.sum(5, 7));
+    }
+}
