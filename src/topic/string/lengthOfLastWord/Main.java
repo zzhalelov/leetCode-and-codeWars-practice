ae3c7e3db7380c90d7598d2b4144aced35971d24
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-12-08 23:00:28 +0600

    Problem #58 (Given a string s consisting of words and spaces, return the length of the last word in the string.)

---
+++ src/topic/string/lengthOfLastWord/Main.java
@@ -0,0 +1,9 @@
+package topic.string.lengthOfLastWord;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.lengthOfLastWord("Hello World");
+        System.out.println(result);
+    }
+}
