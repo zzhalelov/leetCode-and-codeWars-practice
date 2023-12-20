71b85b5dbe6fdc9c6e5253b9f9dba653ecc58281
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-12-20 22:49:07 +0600

    Problem #66 (You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order.  Increment the large integer by one and return the resulting array of digits.)

---
+++ src/topic/math/plusOne/Solution.java
@@ -0,0 +1,33 @@
+package topic.math.plusOne;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        StringBuilder builder = new StringBuilder();
+        String string = null;
+        for (int element : digits) {
+            builder.append(element).append(", ");
+            builder.delete(builder.length() - 2, builder.length()); // Удаляем последнюю запятую и пробел
+            string = builder.toString();
+        }
+        assert string != null;
+        Integer integer = Integer.parseInt(string);
+        Integer newInteger = integer + 1;
+        String newString = Integer.toString(newInteger);
+        char[] charArray = newString.toCharArray();
+        int[] intArray = new int[charArray.length];
+        for (int i = 0; i < charArray.length; i++) {
+            intArray[i] = Character.getNumericValue(charArray[i]);
+        }
+
+        System.out.println(Arrays.toString(intArray));
+        return intArray;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {1, 2, 3};
+        Solution solution = new Solution();
+        solution.plusOne(digits);
+    }
+}
