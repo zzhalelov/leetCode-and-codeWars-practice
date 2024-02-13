88e1ce49a76f621ea0e778971cfc34d607a15a7e
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-02-13 23:51:00 +0600

    Task 1148. Write a solution to find all the authors that viewed at least one of their own articles

---
+++ src/topic/sql_50/task_04.sql
@@ -0,0 +1,7 @@
+--Write a solution to find all the authors that viewed at least one of their own articles.
+--Return the result table sorted by id in ascending order.
+
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY id;
