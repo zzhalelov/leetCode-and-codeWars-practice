522b81e3314adf231f4299a70b8516d25d716f14
Zhastilek Zhalelov <zzhalelov@gmail.com>
2024-09-25 22:53:06 +0500

    Task 326. Given an integer n, return true if it is a power of three. Otherwise, return false. An integer n is a power of three, if there exists an integer x such that n == 3x.

---
+++ src/topic/math/powerOfThree/PowerOfThree.java
@@ -0,0 +1,17 @@
+package topic.math.powerOfThree;
+
+public class PowerOfThree {
+    public static boolean isPowerOfThree(int n) {
+        if (n == 1) {
+            return true;
+        }
+        if (n == 0 || n % 3 != 0) {
+            return false;
+        }
+        return isPowerOfThree(n / 3);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(27));
+    }
+}
