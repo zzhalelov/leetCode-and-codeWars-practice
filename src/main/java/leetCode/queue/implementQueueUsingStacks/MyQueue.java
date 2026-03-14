a0cdeda0097c9f79ac4b8e7c8888e28098cf3b50
Zhastilek Zhalelov <zzhalelov@gmail.com>
2026-03-14 17:06:04 +0500

    queue: 232. Implement Queue using Stacks

--- src/main/java/leetCode/queue/implementQueueUsingStacks/MyQueue.java
+++ src/main/java/leetCode/queue/implementQueueUsingStacks/MyQueue.java
@@ -1,4 +1,39 @@
 package leetCode.queue.implementQueueUsingStacks;
 
+import java.util.Stack;
+
 public class MyQueue {
+    private Stack<Integer> inStack;
+    private Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        moveInToOut();
+        return outStack.pop();
+    }
+
+    public int peek() {
+        moveInToOut();
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.empty();
+    }
+
+    private void moveInToOut() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
 }
