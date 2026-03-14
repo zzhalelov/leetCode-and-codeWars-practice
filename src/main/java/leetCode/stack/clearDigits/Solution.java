ff7c7ac93811307ea502bd160ccf629ba1b27737
Zhastilek Zhalelov <zzhalelov@gmail.com>
2026-03-14 17:01:48 +0500

    stack: 3174. Clear Digits

--- src/main/java/leetCode/stack/clearDigits/Solution.java
+++ src/main/java/leetCode/stack/clearDigits/Solution.java
@@ -1,4 +1,34 @@
 package leetCode.stack.clearDigits;
 
+import java.util.Stack;
+
+/**
+ * 3174. Clear Digits
+ * <p>
+ * You are given a string s.
+ * <p>
+ * Your task is to remove all digits by doing this operation repeatedly:
+ * <p>
+ * Delete the first digit and the closest non-digit character to its left.
+ * Return the resulting string after removing all digits.
+ * </p>
+ * <a href="https://leetcode.com/problems/clear-digits/submissions/1926771350/?envType=problem-list-v2&envId=stack">Submission link</a>
+ */
 public class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                stack.push(ch);
+                sb.append(ch);
+            } else if (!stack.isEmpty() && Character.isDigit(ch)) {
+                stack.pop();
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+
+        return sb.toString();
+    }
 }
--- src/test/java/leetCode/stack/clearDigits/SolutionTest.java
+++ src/test/java/leetCode/stack/clearDigits/SolutionTest.java
@@ -1,4 +1,21 @@
+package leetCode.stack.clearDigits;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SolutionTest {
-  
+    Solution solution = new Solution();
+
+    @Test
+    public void test1() {
+        String s = "abc";
+        assertEquals("abc", solution.clearDigits(s));
+    }
+
+    @Test
+    public void test2() {
+        String s = "cb34";
+        assertEquals("", solution.clearDigits(s));
+    }
 }
